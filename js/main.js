de10ad74337f8051414b20d428be47d348f105f9
Steve Grossi <me@stevegrossi.com>
2016-02-06 10:21:49 -0500

    Jekyll 3!
    - Guess I should have been using the github-pages gem, as it’s now necessary for fenced code block support
    - Now using Kramdown with Github-flavored markdown
    - Now using rouge for JS-less syntax highlighting, so no need for prettify (or any JS at all!)

--- js/main.js
+++
@@ -1,7 +0,0 @@
-var code = document.getElementsByTagName('pre');
-for (var i = 0; i < code.length; i++) {
-  if (code[i].parentNode.className.indexOf('line-data') === -1) {
-    code[i].className = 'prettyprint';
-  }
-}
-prettyPrint();
